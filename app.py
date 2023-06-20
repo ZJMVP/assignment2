b007f0169452dbbc74b0eaaf9afc35278dc212cb
zjmvp <1016686103@qq.com>
2023-06-20 08:11:42 +0800

    add files

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from math import radians, sin, cos, asin, sqrt
-from datetime import date, datetime, timedelta
 from flask import Flask, render_template, request
 import pyodbc
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'zjmvp'
 
 driver = '{ODBC Driver 18 for SQL Server}'
-database = 'zjtest'
+database = 'zj-earth'
 server = 'zj-server.database.windows.net'
 username = "jie_zhao"
 password = "Kd1016686103"
@@ -19,7 +15,7 @@ with pyodbc.connect(
         'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
     with conn.cursor() as cursor:
         temp = []
-        cursor.execute("SELECT TOP 3 time, place FROM earthquake")
+        cursor.execute("SELECT TOP 3 time, id FROM earthquake")
         while True:
             r = cursor.fetchone()
             if not r:
@@ -33,79 +29,92 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/Task1", methods=['GET', 'POST'])
-def task1():
-    return render_template('Task1.html')
+@app.route('/mag', methods=['GET', 'POST'])
+def mag():
+    if request.method == 'POST':
+        mag = request.form["mag"]
+        query = "SELECT * FROM dbo.earthquake where mag >=" + mag
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return render_template('showMag.html', rows=results, temp=0, cnt=len(results))
+    return render_template('ShowMag.html', temp=1)
 
 
-class MyForm1(FlaskForm):
-    mag = StringField(label='Enter Magnitude: ', validators=[DataRequired()])
-    submit = SubmitField(label='Submit')
+@app.route('/searchRange', methods=['GET', 'POST'])
+def searRange():
+    if request.method == 'POST':
+        Range1 = str(request.form['Range1'])
+        Range2 = str(request.form['Range2'])
+        Fromdate = request.form['Fromdate']
+        Todate = request.form['Todate']
+        query = "SELECT * FROM dbo.earthquake where (mag BETWEEN '" + Range1 + "' and '" + Range2 + "') and (CAST(time as date) BETWEEN CAST('" + Fromdate + "' as date) and CAST('" + Todate + "' as date)) "
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return render_template("searchRange.html", length=len(results), rows=results)
+    else:
+        return render_template("searchRange.html")
 
 
 def distance(lat1, lat2, lon1, lon2):
-    # The math module contains a function named
-    # radians which converts from degrees to radians.
     lon1 = radians(lon1)
     lon2 = radians(lon2)
     lat1 = radians(lat1)
     lat2 = radians(lat2)
-    # Haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    # Radius of earth in kilometers. Use 3956 for miles
     r = 6371
-    # calculate the result
-    return (c * r)
+    return c * r
 
 
-@app.route('/Task2', methods=['GET', 'POST'])
-def Task2():
+@app.route('/speLocation', methods=['POST', 'GET'])
+def sLocation():
     if request.method == 'POST':
-        Range1 = str(request.form['Range1'])
-        Range2 = str(request.form['Range2'])
-        net = request.form['Net']
-        query = "SELECT * FROM dbo.earthquake where (mag BETWEEN '" + Range1 + "' and '" + Range2 + "') and net = '" + net + "' order by mag"
+        lat = request.form['lat']
+        lon = request.form['lon']
+        distan = request.form['distance']
+        query = "Select time,depth,latitude,longitude,id,mag,place from earthquake"
         cursor.execute(query)
-        raw_results = cursor.fetchall()
-        length = len(raw_results)
-        results = raw_results[-3:]
-        results.append(raw_results[0])
-        results.append(raw_results[1])
-        results.append(raw_results[2])
-        return render_template("Task2.html", length=len(results), rows=results)
+        lat = float(lat)
+        lon = float(lon)
+        distan = float(distan)
+        rows = cursor.fetchall()
+        results = []
+        for row in rows:
+            x = distance(lat, float(row[2]), lon, float(row[3]))
+            if x <= distan:
+                results.append(row)
+        return render_template("speLocation.html", rows=results, length=len(results))
     else:
-        return render_template("Task2.html")
+        return render_template('speLocation.html')
 
 
-@app.route('/show_a_earthquake', methods=['GET', 'POST'])
-def Task1():
-    if request.method == 'POST':
-        time = request.form['time']
-        query = "SELECT * FROM dbo.earthquake WHERE time = " + time
-        cursor.execute(query)
-        results = cursor.fetchall()
-        return render_template("Task1.html", length=len(results), rows=results)
+@app.route("/cluster", methods=['GET', 'POST'])
+def Cluster():
+    query = "SELECT mag,COUNT(*) FROM earthquake  group by mag"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return render_template("Cluster.html", msg="completed", rows=results)
 
 
-@app.route('/Task3', methods=['GET', 'POST'])
-def Task3():
-    if request.method == 'POST':
-        time = request.form['Time']
-        net = request.form['Net']
-        latitude = request.form['Latitude']
-        longitude = request.form['Longitude']
-        mag = request.form['Mag']
-        place = request.form['Place']
-        insert_row = "INSERT INTO dbo.earthquake VALUES('" + time + "','" + latitude + "','" + longitude + "','" + mag + "','" + net + "','" + place + "')"
-        cursor.execute(insert_row)
-
-        return render_template("index.html")
-    else:
-        return render_template("Task3.html")
+@app.route('/comparison', methods=['POST', 'GET'])
+def comparison():
+    begin = "06:00:00.0000000 +00:00"
+    end = "18:00:00.0000000 +00:00"
+    query1 = "SELECT id FROM dbo.earthquake where mag > 4.0 and (CAST(time as time) BETWEEN CAST('" + begin + "' as time) and CAST('" + end + "' as time)) "
+    cursor.execute(query1)
+    days = cursor.fetchall()
+    earthDayCount = len(days)
+    query2 = "SELECT id FROM dbo.earthquake where mag > 4.0"
+    cursor.execute(query2)
+    wholeDay = cursor.fetchall()
+    earthCount = len(wholeDay)
+    winner = "Night"
+    if earthDayCount > (earthCount - earthDayCount):
+        winner = "Day"
+    return render_template("Comparison.html", dayCount=earthDayCount, nightCount=earthCount - earthDayCount, winner=winner)
 
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True)
